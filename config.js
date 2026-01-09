c49c378d83aa8b60722c2842b21b361f57e95bd8
balhaddad-sys <130838475+balhaddad-sys@users.noreply.github.com>
2026-01-09 20:02:18 +0400

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 
 const CONFIG = {
     // Apps Script Web App deployment URL
-    apiUrl: 'https://script.google.com/macros/s/AKfycbz7ITWWgB6_5VIv_z9zgnWxh1_hdTDe3F89G3MewD5Vfx45STRRyLPbDmNnvCxX3_76/exec',
+    apiUrl: 'https://script.google.com/macros/s/AKfycbxtGGa4zND6EbA9SP1GN7xpLv7pBUC3Jq8Dta2Bu1zaD--pxLEg6RP1PeiptftltfC1/exec',
     
     // Wards
     wards: ["Ward 20", "Ward 21", "Ward 22", "Ward 5", "Ward 27", "Ward 4", "Ward 19", "Ward 10", "ICU", "ER", "Unassigned"],
